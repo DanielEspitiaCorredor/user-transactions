66fb7781ef2e351ffb19b6e87753f44dcaec1764
Daniel Espitia Corredor <danielespitiacorredor@gmail.com>
2024-09-25 02:30:14 -0500

    fix: issues when insert records

--- user_transactions/transaction/v1/db.py
+++ user_transactions/transaction/v1/db.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import Iterator
+from pymongo.errors import BulkWriteError
 
 from user_transactions.models.v1.transaction import Transaction
 
@@ -12,11 +13,13 @@ async def insert_transactions(itrecords: Iterator, datasize: int):
     data_to_insert = []
     
     for i in range(datasize):
-        
-        doc = next(itrecords)
-        data_to_insert.append(Transaction(**doc))
-        
-        if len(data_to_insert) >= insert_limit or i == datasize -1:
+        try:
+            doc = next(itrecords)
+            data_to_insert.append(Transaction(**doc))
             
-            await Transaction.insert_many(data_to_insert)
+            if len(data_to_insert) >= insert_limit or i == datasize -1:
+                
+                await Transaction.insert_many(data_to_insert)
+                data_to_insert = []
+        except BulkWriteError as e:
             data_to_insert = []
